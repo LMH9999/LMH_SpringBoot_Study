f1bf4983fb2b5e2dee62bdb3bca57e6b9719414d
LMH <leemh9256@gmail.com>
2024-01-24 17:40:47 +0900

    에러처리 실습

---
+++ 24_01_24/에러처리/error-handle/src/main/java/kr/co/chunjae/controller/StatisticsController.java
@@ -0,0 +1,27 @@
+package kr.co.chunjae.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.concurrent.TimeoutException;
+
+@Slf4j
+@Controller
+@RequestMapping("statistics")
+@RequiredArgsConstructor
+public class StatisticsController {
+
+    @ExceptionHandler(value = Exception.class)
+    public String whtToDoException() throws Exception{
+        return "notfound";
+    }
+
+    @ExceptionHandler(value = TimeoutException.class)
+    public String notFoundException() throws Exception{
+        return "timeoutException";
+    }
+}
