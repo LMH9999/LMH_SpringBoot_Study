85f86af6092fef1e812094b9e21703517f3cdbf3
LMH <leemh9256@gmail.com>
2024-01-26 17:01:57 +0900

    Ajax 기본 사용법

---
+++ 24_01_26/Ajax/ajaxEx/src/main/java/com/study/springboot/dto/AjaxDTO.java
@@ -0,0 +1,14 @@
+package com.study.springboot.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class AjaxDTO {
+
+    private String param1;
+    private String param2;
+}
